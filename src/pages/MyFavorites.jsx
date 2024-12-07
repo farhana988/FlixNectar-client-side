0effe48077ea6105491ac2149590df249a3ce1e2
farhana988 <farhana744255@gmail.com>
2024-12-07 23:09:13 +0600

    no data text added in favorite list

--- src/pages/MyFavorites.jsx
+++ src/pages/MyFavorites.jsx
@@ -1,39 +1,47 @@
 // import React from 'react';
 
-import {  useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 import FavoriteCard from "../components/FavoriteCard";
 import Heading from "../components/Heading";
 import { useState } from "react";
+import { TfiFaceSad } from "react-icons/tfi";
 
 const MyFavorites = () => {
-    const favorites = useLoaderData()
-   
-    const[loadedFavorite, setLoadedFavorite]=useState(favorites)
+  const favorites = useLoaderData();
 
+  const [loadedFavorite, setLoadedFavorite] = useState(favorites);
 
+  return (
+    <div className="container mx-auto  py-10">
+      <Heading
+        title={"My Favorite Films"}
+        subtitle={
+          "Here’s a collection of movies that have left a lasting impact on me. Whether its the storytelling, characters, or visuals, these films hold a special place in my heart."
+        }
+      ></Heading>
 
-    return (
-        <div className="container mx-auto  py-10">
-          <Heading
-          title={'My Favorite Films'}
-          subtitle={'Here’s a collection of movies that have left a lasting impact on me. Whether its the storytelling, characters, or visuals, these films hold a special place in my heart.'}
-          ></Heading>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-5 lg:px-0 ">
+      {loadedFavorite.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-5 lg:px-0">
           {loadedFavorite.map((favorite) => (
-           <FavoriteCard
-           favorite={favorite}
+            <FavoriteCard
+              favorite={favorite}
               loadedFavorite={loadedFavorite}
               setLoadedFavorite={setLoadedFavorite}
               key={favorite._id}
             ></FavoriteCard>
           ))}
-          
         </div>
-        
+      ) : (
+        <div className="flex flex-col justify-center items-center gap-5 my-40 active
+        text-xl md:text-2xl lg:text-4xl font-bold">
+          <TfiFaceSad className="text-9xl " />
+          <p className="opacity-50">No favorites found.</p>
+          <p className="opacity-50">Start adding some movies to your list!</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default MyFavorites;
